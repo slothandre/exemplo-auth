5843ed38e88c2646129150f15bbc7c8bc5e3a73d
André Marques <andre.m.d.abreu@gmail.com>
2024-03-19 11:25:36 -0300

    Update Cadastro.js

--- src/screens/Cadastro.js
+++ src/screens/Cadastro.js
@@ -1,20 +1,30 @@
 import { auth } from "../../firebase.config";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { useState } from "react";
 import { Alert, Button, StyleSheet, TextInput, View } from "react-native";
 
 export default function Cadastro({ navigation }) {
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
+  const [nome, setNome] = useState("");
 
   const cadastrar = async () => {
-    if (!email || !senha) {
-      Alert.alert("Atenção", "Preencha e-mail e senha!");
+    if (!email || !senha || !nome) {
+      Alert.alert("Atenção", "Preencha nome, e-mail e senha!");
       return;
     }
 
     try {
-      await createUserWithEmailAndPassword(auth, email, senha);
+      const contaUsuario = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        senha
+      );
+
+      if (contaUsuario.user) {
+        await updateProfile(auth.currentUser, { displayName: nome });
+        console.log(contaUsuario.user.displayName);
+      }
 
       Alert.alert("Cadastro", "Seu cadastro foi concluído com sucesso!", [
         {
@@ -54,21 +64,29 @@ export default function Cadastro({ navigation }) {
     }
   };
   return (
-    <View style={estilos.container}>
-      <View style={estilos.formulario}>
+    <View style={styles.container}>
+      <View style={styles.formulario}>
+        <TextInput
+          placeholder="Nome"
+          style={styles.input}
+          keyboardType="default"
+          onChangeText={(valor) => {
+            setNome(valor);
+          }}
+        />
         <TextInput
           placeholder="E-mail"
-          style={estilos.input}
+          style={styles.input}
           keyboardType="email-address"
           onChangeText={(valor) => setEmail(valor)}
         />
         <TextInput
           placeholder="Senha"
-          style={estilos.input}
+          style={styles.input}
           secureTextEntry
           onChangeText={(valor) => setSenha(valor)}
         />
-        <View style={estilos.botoes}>
+        <View style={styles.botoes}>
           <Button onPress={cadastrar} title="Cadastre-se" color="blue" />
         </View>
       </View>
@@ -76,7 +94,7 @@ export default function Cadastro({ navigation }) {
   );
 }
 
-const estilos = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "lightblue",
